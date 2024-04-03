340975a6f211e85f5f343ce1fb017fbc86c0c58a
Andrew Bergman <andrew.jg.bergman@gmail.com>
2024-04-04 01:31:48 +0200

    preprocessing

--- preprocessing.py
+++ preprocessing.py
@@ -15,7 +15,9 @@ def load_data_file(abundance_file, metadata_file) :
     return abundance_df, meta_df
 
 def import_coordinates(abundance_df, meta_df) :
-    coordinates = meta_df[['uuid', 'city_longitude', 'city_latitude']] #Retrieve all UUIDs and their corresponding coordinates
+    coordinates = meta_df[['uuid', 'longitude', 'latitude']] #Retrieve all UUIDs and their corresponding coordinates
+    abundance_df['uuid'] = abundance_df['uuid'].astype(str)
+    coordinates['uuid'] = coordinates['uuid'].astype(str)
     df = pd.merge(abundance_df, coordinates, on='uuid', how="inner")
     return df
 
@@ -23,7 +25,7 @@ def calculate_vif_single_feature(data, feature_index):
     return variance_inflation_factor(data.values, feature_index)
 
 def calculate_vif(predictors_df):
-    predictors_df.drop(["uuid", "city_longitude_y", "city_latitude_y"], axis=1, inplace=True)
+    predictors_df.drop(["uuid", "longitude", "latitude"], axis=1, inplace=True)
     # Convert all columns to numeric, forcing non-convertible values to NaN
     predictors_df = predictors_df.apply(pd.to_numeric, errors='coerce')
     # Drop rows with NaN values to ensure clean VIF calculation
@@ -42,12 +44,43 @@ def calculate_vif(predictors_df):
     vif_data = pd.DataFrame()
     vif_data["Feature"] = features
     vif_data["VIF"] = vifs
-    vif_df.to_csv("vif_df.csv", index=False)
+    vif_data.to_csv("vif_df.csv", index=False)
 
     return vif_data
 
+def generate_corr_matrix(df):
+    corr_mtx = df.corr()
+    print(corr_mtx)
+    return corr_mtx
 
-abundance_df, meta_df = load_data_file(metadata_file="./complete_metadata.csv", abundance_file="./first_500")
+def drop_columns(corr_matrix) :
+    # Create an empty set to keep track of columns to drop
+    columns_to_drop = set()
+
+    # Iterate over the correlation matrix
+    for i in range(len(corr_matrix.columns)):
+        for j in range(i):
+            if abs(corr_matrix.iloc[i, j]) > 0.6: # The optimal threshold seem to lie between 0.5 and 0.7. 0.7 yields RuntimeWarning: divide by zero encountered in scalar divide vif = 1. / (1. - r_squared_i)
+                # Add the column name to our set
+                colname = corr_matrix.columns[i]
+                columns_to_drop.add(colname)
+
+    print(columns_to_drop)
+    df_dropped = df.drop(columns=columns_to_drop)
+    return df_dropped
+
+
+abundance_df, meta_df = load_data_file(metadata_file="./complete_metadata.csv", abundance_file="./first_1000")
 df = import_coordinates(abundance_df, meta_df)
+
+new_df = df.iloc[:, 1:-2]
+new_df = new_df.loc[:, (df != 0).any(axis=0)]
+
+new_df.to_csv("selected_predictors")
+
+#print(new_df)
+corr_mtx = generate_corr_matrix(new_df)
+df = drop_columns(corr_mtx)
+
 vif_df = calculate_vif(df)
 
