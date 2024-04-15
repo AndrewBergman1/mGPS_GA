017d3cde1b5aa0646625b117db79584b99569a09
Andrew Bergman <andrewb@cosmos1.lunarc.lu.se>
2024-04-15 17:01:59 +0200

    Adding all

---
+++ batch_script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH -t 24:00:00
+#SBATCH --mail-user=andrew.jg.bergman@gmail.com
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+#SBATCH -J GA_run_0.3_0.9_0.2_20_20_2_20_3
+
+# Load the module for Anaconda/Miniconda if it's not automatically initialized
+module load Anaconda3/2024.02-1  # Adjust as per module available on your cluster
+
+# Activate your Conda environment
+source activate /home/andrewb/miniconda3/envs/GA_feature_selection
+
+# Run your Python script
+python /home/andrewb/GA_feature_selection/single_core_test.py 0.3 0.9 0.2 20 20 2 20 3 || echo "Script failed with exit code $?"
